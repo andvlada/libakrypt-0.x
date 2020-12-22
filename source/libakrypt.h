6955b80702b55584f1fcd1e2d6fdf4fb1f8d1274
andvlada <58373801+andvlada@users.noreply.github.com>
2020-12-22 18:30:08 +0300

    updated xorshift

--- source/libakrypt.h
+++ source/libakrypt.h
@@ -440,6 +440,9 @@ extern "C" {
  dll_export int ak_random_ptr( ak_random , const ak_pointer , const ssize_t );
 /*! \brief Некриптографическая функция генерации случайного 64-х битного целого числа. */
  dll_export ak_uint64 ak_random_value( void );
+ /*! \brief Инициализация начальных значений */
+ ak_uint32 strt;
+ ak_uint64 start, start_2;
  /*! \brief xorshift функция генерации случайного 32-х битного целого числа. */
  dll_export ak_uint64 ak_xorshift32(void);
  /*! \brief xorshift функция генерации случайного 64-х битного целого числа. */
